a50e2916e920ab94bb39662589749474ebe0c865
subhanshaik2025 <subhan.shaik1207@gmail.com>
2026-06-10 20:21:24 +0530

    fix: remove duplicate theme import

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { GOLD, BG, SURF, BOR, TX, MU, DIM } from './utils/theme';
 import { registerUser, loginUser } from './auth';
 import { verifyPhoneNumberFromSheet } from './googleSheets';
 import { INDUSTRIES } from './config';
